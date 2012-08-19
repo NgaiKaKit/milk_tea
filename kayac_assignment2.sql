1f2e3811ca2dc434eb31d9abf7c9e40362819baa
hnomka <hnomka@gmail.com>
2012-08-20 03:36:04 +0900

    upload SQL

---
+++ kayac_assignment2.sql
@@ -0,0 +1,140 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 18, 2012 at 07:04 PM
+-- Server version: 5.5.27
+-- PHP Version: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `kayac_assignment2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group`
+--
+
+CREATE TABLE IF NOT EXISTS `group` (
+  `group_id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `total_topic` int(11) NOT NULL,
+  `last_topic_id` int(11) NOT NULL,
+  PRIMARY KEY (`group_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
+
+--
+-- Dumping data for table `group`
+--
+
+INSERT INTO `group` (`group_id`, `group_name`, `total_topic`, `last_topic_id`) VALUES
+(0, 'å…¨éƒ¨', 0, 0),
+(8, 'ãƒ‹ãƒ¥ãƒ¼ã‚¹', 0, 0),
+(11, 'æ—…è¡Œ', 0, 0),
+(12, 'å‹•ç‰©', 0, 0),
+(13, 'æ–™ç†', 0, 0),
+(17, 'ã‚²ãƒ¼ãƒ ', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reply`
+--
+
+CREATE TABLE IF NOT EXISTS `reply` (
+  `reply_id` int(11) NOT NULL AUTO_INCREMENT,
+  `topic_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `reply_time` datetime NOT NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`reply_id`),
+  KEY `post_id` (`topic_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=49 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `topic`
+--
+
+CREATE TABLE IF NOT EXISTS `topic` (
+  `topic_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `topic_title` varchar(1000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `post_time` datetime NOT NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `reply_count` int(11) NOT NULL DEFAULT '0',
+  `last_reply_people` int(11) NOT NULL DEFAULT '0',
+  `last_reply_time` datetime DEFAULT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`topic_id`),
+  UNIQUE KEY `topic_id` (`topic_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=244 ;
+
+--
+-- Dumping data for table `topic`
+--
+
+INSERT INTO `topic` (`topic_id`, `user_id`, `topic_title`, `post_time`, `content`, `reply_count`, `last_reply_people`, `last_reply_time`, `group_id`) VALUES
+(0, 0, 'null', '2012-08-16 00:09:30', 'null', 0, 0, NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info`
+--
+
+CREATE TABLE IF NOT EXISTS `user_info` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `type` tinyint(4) NOT NULL,
+  `reg_datetime` datetime NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_name` (`user_name`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=203 ;
+
+--
+-- Dumping data for table `user_info`
+--
+
+INSERT INTO `user_info` (`user_id`, `user_name`, `password`, `email`, `type`, `reg_datetime`) VALUES
+(0, 'null', '', 'null', 0, '2012-08-01 00:00:00'),
+(1, 'admin', '302d70adf617c33881828ae64f1772268ef0b7d1', 'admin@admin.com', 1, '2012-08-08 00:00:00'),
+(2, 'guest', '', 'guest', 2, '2012-08-08 00:00:00');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reply`
+--
+ALTER TABLE `reply`
+  ADD CONSTRAINT `reply_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`),
+  ADD CONSTRAINT `reply_ibfk_3` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`topic_id`);
+
+--
+-- Constraints for table `topic`
+--
+ALTER TABLE `topic`
+  ADD CONSTRAINT `topic_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
